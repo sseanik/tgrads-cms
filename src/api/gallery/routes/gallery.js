8f7111658b6f3289aaa423694a646ae0a2502e64
Sean Smith <sean.smith@student.unsw.edu.au>
2022-06-08 11:26:37 +1000

    Changed models

--- src/api/gallery/routes/gallery.js
+++ src/api/gallery/routes/gallery.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * article router.
+ * gallery router.
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::article.article');
+module.exports = createCoreRouter('api::gallery.gallery');
